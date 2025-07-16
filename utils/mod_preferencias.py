cf5d2b536c6b2639966fbc3eae73b97af04f96f6
Daniel Rodríguez <138172621+DanielRdz4@users.noreply.github.com>
2025-07-15 18:18:15 -0600

    Inicio del módulo de preferencias del usuario

--- utils/mod_preferencias.py
+++ utils/mod_preferencias.py
@@ -1,3 +1,10 @@
 #Módulo para gestionar las preferencias de usuario
 import os
 import shutil
+
+directorio_actual = os.curdir()
+directorio_carpeta = "data"
+ruta = os.path.join(directorio_actual,directorio_carpeta)
+
+if os.path.exists(ruta):
+    pass
